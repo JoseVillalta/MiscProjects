a4210b2deb5065e43ba12f69a9352c93e88c02eb
jvillalt <jvillalta@impinj.com>
2017-06-20 08:04:46 -0700

    Finished the calculating functions with tests

--- PrimaryArithmetic/PrimaryArithmetic/CarryCounter.cpp
+++ PrimaryArithmetic/PrimaryArithmetic/CarryCounter.cpp
@@ -3,12 +3,44 @@
 
 int CalculateNumberCarry(int a, int b)
 {
-    return 0;
+    auto arrA = ConvertToArray(a);
+    auto arrB = ConvertToArray(b);
+
+    int counter = 0;
+    int hasCarry = 0;
+
+    for(int i = 0; i<11; i++)
+    {
+        hasCarry = HasCarry(arrA[i], arrB[i], hasCarry);
+        if(hasCarry == 1)
+        {
+            counter++;
+        }
+    }
+
+    return counter;
 }
 
 int * ConvertToArray(int a)
 {
-    return nullptr;
+    int *  array = new int[11];
+
+    for(int j=0; j<11; j++)
+    {
+        array[j] = 0;
+    }
+   
+    int i = 0;
+    while(a > 0)
+    {
+        int digit = a % 10;
+        array[i] = digit;
+        a -= digit;
+        a = a / 10;
+        i++;
+    }
+
+    return array;
 }
 
 int HasCarry(int a, int b, int c)
--- PrimaryArithmetic/UnitTests/unittest1.cpp
+++ PrimaryArithmetic/UnitTests/unittest1.cpp
@@ -16,11 +16,11 @@ namespace UnitTests
 		}
         TEST_METHOD(HasCarryisTrue)
 		{
-            Assert::AreEqual(HasCarry(5, 5, 0), 1);
+            Assert::AreEqual(1, HasCarry(5, 5, 0));
 		}
         TEST_METHOD(HasCarryisFalse)
 		{
-            Assert::AreEqual(HasCarry(4, 4, 1), 0);
+            Assert::AreEqual(0, HasCarry(4, 4, 1));
 		}
 
 	};
@@ -31,9 +31,57 @@ namespace UnitTests
         {
             int number = 356;
             int* result = ConvertToArray(number);
-            Assert::AreEqual(result[0], 6);
-            Assert::AreEqual(result[1], 5);
-            Assert::AreEqual(result[2], 3);
-        }    
+            Assert::AreEqual( 6, result[0]);
+            Assert::AreEqual( 5, result[1]);
+            Assert::AreEqual( 3, result[2]);
+        }
+        TEST_METHOD(ConvertZero)
+        {
+            int number = 0;
+            int* result = ConvertToArray(number);
+            Assert::AreEqual(0, result[0]);
+        }
+        TEST_METHOD(ConvertLarge)
+        {
+            int number = 1234567890;
+            int* result = ConvertToArray(number);
+            Assert::AreEqual(0, result[0]);
+            Assert::AreEqual(9, result[1]);
+            Assert::AreEqual(8, result[2]);
+            Assert::AreEqual(7, result[3]);
+            Assert::AreEqual(6, result[4]);
+            Assert::AreEqual(5, result[5]);
+            Assert::AreEqual(4, result[6]);
+            Assert::AreEqual(3, result[7]);
+            Assert::AreEqual(2, result[8]);
+            Assert::AreEqual(1, result[9]);
+            Assert::AreEqual(0, result[10]);
+        }
+    };
+    TEST_CLASS(CarryCounterTests)
+    {
+    public:
+        TEST_METHOD(NominalCase)
+        {
+            int a = 3;
+            int b = 2;
+            int result = CalculateNumberCarry(a, b);
+            Assert::AreEqual(0, result);
+        }
+        TEST_METHOD(OneCarryTest)
+        {
+            int a = 5;
+            int b = 6;
+            int result = CalculateNumberCarry(a, b);
+            Assert::AreEqual(1, result);
+        }
+        TEST_METHOD(TwoCarryTest)
+        {
+            int a = 85;
+            int b = 56;
+            int result = CalculateNumberCarry(a, b);
+            Assert::AreEqual(2, result);
+        }
+        
     };
 }
